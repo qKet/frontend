6a9f0660d8adf8cd5c5694416399a0969d430917
MoonJunH <pdu04009@naver.com>
2026-08-21 10:51:55 +0900

    feat: 공연 감상평(REV01) UI 포팅 - Qket_old feature/mjh-REV01_REVIEW01-04

--- app/events/[performanceId]/page.tsx
+++ app/events/[performanceId]/page.tsx
@@ -92,7 +92,7 @@ export default async function PerformanceDetailPage({
       />
 
       {/* 출연진 / 감상평 탭 */}
-      <PerformanceTabs commonCasts={commonCasts} roundCasts={roundCasts} />
+      <PerformanceTabs performanceId={performanceId} commonCasts={commonCasts} roundCasts={roundCasts} />
     </PageHeader>
   );
 }
